2516ce99f3d5bcb9132aacb70546b966e80e0829
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-17 00:34:45 +0300

    render StoriesList component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Error from "next/error";
 import fetch from "isomorphic-fetch";
+import StoriesList from "../components/StoriesList/StoriesList";
 
 const Home = ({ stories }) => {
   return !stories.hits?.length ? (
@@ -8,11 +9,7 @@ const Home = ({ stories }) => {
   ) : (
     <div>
       <h1>home page</h1>
-      <div>
-        {stories.hits?.map((story) => (
-          <h3 key={story.objectID}>{story.title}</h3>
-        ))}
-      </div>
+      <StoriesList stories={stories} />
     </div>
   );
 };
@@ -20,7 +17,7 @@ const Home = ({ stories }) => {
 Home.getInitialProps = async () => {
   let stories;
   try {
-    const response = await fetch("http://hn.algolia.com/api/v1/search?page=1");
+    const response = await fetch("http://hn.algolia.com/api/v1/search?page=0");
     stories = await response.json();
   } catch (error) {
     stories = [];
